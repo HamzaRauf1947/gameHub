908e111ce0436cffbcf10e9bb31d3db194410812
Hamza Rauf <hamzaraufstudy@gmail.com>
2026-03-28 01:04:10 +0500

    move api key to environment variables

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:'https://api.rawg.io/api/',
     params:{
-        key:'e8c1d5f50c4f445a86171eea8c91d998'
+        key:import.meta.env.VITE_RAWG_API_KEY
     }
 })
